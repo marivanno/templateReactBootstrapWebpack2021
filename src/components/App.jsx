0170f3cd12aa16c0bcdf965814e33f85c87af678
Ivan Shadrin <chubakaivan@gmail.com>
2021-10-27 17:15:12 +0300

    start page added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import {
+  Container, Row, Col, Button,
+} from 'react-bootstrap';
 
 const App = () => {
-  const handleClick = () => console.log('you tapped to Button');
+  const handleClick = () => {
+    window.alert('you tapped to Button');
+    console.log('you tapped to Button');
+  };
   return (
-    <Button>Click me</Button>
+    <Container lg className="h-100">
+      <Row className="h-100 justify-content-center align-items-center px-5 py-5">
+        <Col className="align-items-center col-8 shadow">
+          <div className="d-flex flex-column align-items-center my-5">
+            <div className="fs-4 align-items-center badge bg-primary text-wrap">Welcome to template</div>
+            <div className="mb-5">React + Bootstrap</div>
+            <Button onClick={handleClick} variant="danger" size="">tap on me</Button>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
