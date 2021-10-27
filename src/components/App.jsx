a067aa0340a73b4f49bdf6a870dfbd480529b492
Ivan Shadrin <chubakaivan@gmail.com>
2021-10-27 17:43:13 +0300

    some changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,13 +8,13 @@ const App = () => {
     window.alert('you tapped to Button');
     console.log('you tapped to Button');
   };
-  return (
+  return ( 
     <Container lg className="h-100">
       <Row className="h-100 justify-content-center align-items-center px-5 py-5">
-        <Col className="align-items-center col-8 shadow">
-          <div className="d-flex flex-column align-items-center my-5">
+        <Col className="align-items-center col-8 shadow bg-light bg-gradient">
+          <div className="d-flex flex-column align-items-center my-5 ">
             <div className="fs-4 align-items-center badge bg-primary text-wrap">Welcome to template</div>
-            <div className="mb-5">React + Bootstrap</div>
+            <div className="mb-5 fst-italic">React + Bootstrap</div>
             <Button onClick={handleClick} variant="danger" size="">tap on me</Button>
           </div>
         </Col>
